b77e52dc586923e94a2bb81d747a7aac0e1263f6
levitatorius <levitatorius@gmail.com>
2015-07-21 10:52:26 +0200

    Update protocols.swift

--- protocols.swift
+++ protocols.swift
@@ -3,8 +3,17 @@ protocol Feed {
 }
 
 protocol Folder {
-  var feeds:[Feed] { get }
+  var feeds:[Feed] { get set } //needs to be mutable if using protocol extension
   func add(newFeeds:[Feed])
+}
+
+extension Folder {
+  mutating func add(newFeeds:[Feed]) {
+    for newFeed in newFeeds {
+      if !feeds.contains({ $0.url == newFeed.url }) {
+        feeds.append(newFeed)
+    }
+  }
 }
 
 class LocalFolder: Folder {
