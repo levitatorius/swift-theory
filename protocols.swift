6958cd50898644afb12b3c63e2e757ebd489d2c1
levitatorius <levitatorius@users.noreply.github.com>
2015-07-21 22:01:52 +0200

    Update protocols.swift

--- protocols.swift
+++ protocols.swift
@@ -1,26 +1,45 @@
 protocol Feed {
-  var url:String { get }
+	var url:String { get }
 }
 
 protocol Folder {
-  var feeds:[Feed] { get set } //needs to be mutable if using protocol extension
-  func add(newFeeds:[Feed])
+	var feeds:[Feed] {get set} //needs to be mutable if using protocol extension
+	func add(newFeeds:[Feed])
 }
 
-//default implementation, optional
+// default implementation, optional
 extension Folder {
-  mutating func add(newFeeds:[Feed]) {
-    for newFeed in newFeeds {
-      if !feeds.contains({ $0.url == newFeed.url }) {
-        feeds.append(newFeed)
-    }
-  }
+	mutating func add(newFeeds:[Feed]) {
+		for newFeed in newFeeds {
+			if !feeds.contains({ $0.url == newFeed.url }) {
+				feeds.append(newFeed)
+			}
+		}
+	}
 }
 
-//concrete implementation
+// concrete implementation
 class LocalFolder: Folder {
-  var feeds = [Feed]()
-  func add(newFeeds:[Feed]) {
-    feeds += newFeeds.filter { newFeed in !self.feeds.contains { $0.url == newFeed.url } }
-  }
+	var feeds = [Feed]()
+	func add(newFeeds:[Feed]) {
+		feeds += newFeeds.filter { newFeed in !self.feeds.contains { $0.url == newFeed.url } }
+	}
 }
+
+struct sA: Feed {
+	var url = "StructA"
+}
+
+class sB: Feed {
+	var url = "StructB"
+}
+
+struct sC: Feed {
+	var url = "StructC"
+}
+
+var tf = LocalFolder()
+var arr:[Feed] = [sA(), sB(), sC()]
+tf.add([sA(), sB(), sC()])
+tf.add([sA()])
+print(tf.feeds)
